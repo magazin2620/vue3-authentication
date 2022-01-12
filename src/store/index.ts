d27a93cd755863d19a7f829b2c49aed8e969a175
magazin2620 <magazin2620@gmail.com>
2022-01-12 13:16:47 +0500

    initial

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,15 @@
-import { createStore } from 'vuex'
+import {Commit, createStore } from 'vuex'
+
 
 export default createStore({
-  state: {
+	state: {
+	authenticated:false,
   },
-  mutations: {
+	mutations: {
+	SET_AUTH:(state:{authenticated:boolean},auth:boolean)=>state.authenticated=auth
   },
-  actions: {
+	actions: {
+	setAuth:({commit}:{commit:Commit},auth:boolean)=>commit('SET_AUTH',auth)
   },
   modules: {
   }
